0ed229358c1edf946e436cd4a69a17835384d7f0
IJE2704 <mh1669101@gmail.com>
2023-06-15 09:11:40 +0600

    add searching data & spinig

--- js/app.js
+++ js/app.js
@@ -1,23 +1,50 @@
-const phoneLoader = async()=>
-{
-    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`
+const phoneLoader = async (text) => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${text}`
     const res = await fetch(url);
     const data = await res.json();
     phoneDisplay(data.data);
 }
-phoneLoader();
-const phoneDisplay = phones =>{
+const phoneDisplay = phones => {
     const phoneContainer = document.getElementById('phone-container');
-    phones.forEach(phone =>{
-        const phoneDiv = document.createElement('div');
-        phoneDiv.classList.add('col');
-        phoneDiv.innerHTML = `
-        <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-        `
-    })
+    phoneContainer.innerHTML = ``;
+    phones = phones.slice(0, 10);
+    const noFound = document.getElementById('no-found');
+    if (phones.length === 0) {
+        noFound.classList.remove('d-none');
+    }
+    else {
+        noFound.classList.add('d-none');
+        phones.forEach(phone => {
+            const phoneDiv = document.createElement('div');
+            phoneDiv.classList.add('col');
+            phoneDiv.innerHTML = `
+            <div class="card p-5">
+                <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                     <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            `;
+            phoneContainer.appendChild(phoneDiv);
+        })
+    }
+    isSpining(false);
+}
+document.getElementById('btn-search').addEventListener('click', function () {
+    const searchField = document.getElementById('search-field');
+    const serachText = searchField.value;
+    phoneLoader(serachText);
+    searchField.value = '';
+    isSpining(true);
+})
+
+const isSpining = spin =>{
+    const spinField = document.getElementById('spin-field');
+    if(spin)
+    {
+        spinField.classList.remove('d-none');
+    }
+    else{
+        spinField.classList.add('d-none');
+    }
 }
